192950b98abc75b2e21463f53654482c40123db2
Carlos Machado <carlosmachadojr@outlook.com>
2022-12-03 21:51:31 -0300

    initial commit

---
+++ src/test/java/br/com/cmachado/cashflowcontrol/CashFlowControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cmachado.cashflowcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
